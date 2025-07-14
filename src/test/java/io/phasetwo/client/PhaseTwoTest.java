c13ee99b795c4e74d7051ef5a2d70f690af95737
Luca Tronchin <ltronky@gmail.com>
2025-07-14 08:42:28 +0200

    upgrade deps

--- src/test/java/io/phasetwo/client/PhaseTwoTest.java
+++ src/test/java/io/phasetwo/client/PhaseTwoTest.java
@@ -15,7 +15,7 @@ import java.util.List;
 public class PhaseTwoTest {
 
   public static final KeycloakContainer container =
-      new KeycloakContainer("quay.io/phasetwo/phasetwo-keycloak:25.0.4").withContextPath("/auth").withReuse(true);
+      new KeycloakContainer("quay.io/phasetwo/phasetwo-keycloak:26.2.5").withContextPath("/auth").withReuse(true);
 
   @BeforeAll
   public static void beforeAll() {
